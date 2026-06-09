e12e786d3cbb867f4adf0606a94a8b899bd0a3a6
Darshan G.V <darshangv36@gmail.com>
2026-06-09 23:26:25 +0530

    single number

---
+++ 136. Single Number.cpp
@@ -0,0 +1,31 @@
+// time complexity: O(n)
+// space complexity: O(1)
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int one=0;
+        for(int i=0; i<nums.size(); i++){
+            one=one^nums[i];
+        }https://leetcode.com/problems/single-number/
+        return one;
+    }
+};
+
+   int main(){
+    Solution obj;
+    int n;
+    cin>>n;
+    vector<int>res(n);
+    for(int i=0; i<n; i++){
+        cin>>res[i];
+    }
+    int ans = obj.singleNumber(res);
+    cout<<ans;
+    return 0;
+   }
