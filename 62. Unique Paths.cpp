bb53e35448609c91248eff5a2c373f67962aa2d3
Darshan G.V <darshangv36@gmail.com>
2026-08-01 15:28:12 +0530

    uni

---
+++ 62. Unique Paths.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n,1);
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[j]=dp[j]+dp[j-1];
+            }
+        }
+        return dp[n-1];
+    }
+};
+
+int main(){
+    Solution obj;
+    int m , n;
+    cin>>m>>n;
+    int ans = obj.uniquePaths(m,n);
+    cout<<ans;
+    return 0;
+}
