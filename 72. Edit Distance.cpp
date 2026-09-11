a900b2bdb8c383dae165b4fdcc48c28a6f355068
darshan <darshangv36@gmail.com>
2026-09-11 20:50:29 +0530

    dp

---
+++ 72. Edit Distance.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        if(word1.size()==0){
+            return word2.size();
+        }
+        int n=word1.size(),m=word2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=0; i<=n; i++){
+            dp[i][0]=i;
+        }
+        for(int j=0; j<=m; j++){
+            dp[0][j]=j;
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                  dp[i][j] = 1 + min(
+    dp[i-1][j],
+    min(dp[i][j-1], dp[i-1][j-1])
+);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
+
+
+  int main(){
+    Solution obj;
+    string res1, res2;
+    getline(cin,res1);
+    getline(cin,res2);
+    int ans=obj.minDistance(res1,res2);
+    cout<<ans;
+    return 0;
+  }
